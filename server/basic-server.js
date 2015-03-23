50d0528cce25196f66d0e3d8ea55b99bf4dfee45
MKS-Student <mks.student@hackreactor.com>
2015-03-23 15:18:36 -0700

    Exports and requires added.

--- server/basic-server.js
+++ server/basic-server.js
@@ -14,7 +14,7 @@ var port = 3000;
 // special address that always refers to localhost.
 var ip = "127.0.0.1";
 
-
+var handler = require('./request-handler.js');
 
 // We use node's http module to create a server.
 //
@@ -22,12 +22,12 @@ var ip = "127.0.0.1";
 // incoming requests.
 //
 // After creating the server, we will tell it to listen on the given port and IP. */
-// var server = http.createServer(handleRequest);
-var server = http.createServer(function(request, response) {
-  response.writeHead(200);
-  response.write("Hello, this is Paul.");
-  response.end();
-});
+var server = http.createServer(handler.handlerRequest);
+// var server = http.createServer(function(request, response) {
+//   response.writeHead(200);
+//   response.write("Hello, this is Paul.");
+//   response.end();
+// });
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
